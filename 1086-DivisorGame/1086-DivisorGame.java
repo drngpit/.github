0714dda2096973bbbe01d53392dfe94071e9bbed
BALAJI V <bala2327143@gmail.com>
2026-02-09 13:56:50 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,13 @@
+// Last updated: 09/02/2026, 13:56:49
+class Solution {
+    public boolean divisorGame(int n) {
+        
+    
+        return n % 2 == 0; 
+    }
+
+    }
+
+            
+        
+    
