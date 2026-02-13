41abb7956e20e40a9181baaa3346a87231a47122
MadheshN11 <ningarajumadhesh@gmail.com>
2026-02-13 10:37:46 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,18 @@
+// Last updated: 2/13/2026, 10:37:38 AM
+class Solution {
+    public String toLowerCase(String s) {
+     
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
+
+        
+    
