41fc8987adf625c7de0196d6bae93d7c0f06253c
krishnakumar-5 <145209887+krishnakumar-5@users.noreply.github.com>
2026-02-13 10:18:01 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,9 @@
+// Last updated: 2/13/2026, 10:18:02 AM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
+    }
+}
